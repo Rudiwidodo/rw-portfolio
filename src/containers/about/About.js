4df11481b8def039072c40148bb7145656d418ca
Erway <rudiwidodo069@gmail.com>
2023-07-10 11:26:04 +0700

    add project portfolio to github

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -2,39 +2,43 @@ import React from 'react';
 import './About.css';
 
 export default function About() {
-    return (
-        <div className='about-container' id='About'>
-            <div className='about-parent'>
-                <div className='about-title'>
-                    <h3>
-                        About
-                    </h3>
-                    <div className='line'></div>
-                </div>
-                <div className='about-card'>
-                    <div className='row'>
-                        <div className='col-md-6 col-left'>
-                            <div className='card-image-about'>
-                                <img src={require('../../assets/images/profile/profile.jpeg')} alt={""} />
-                            </div>
-                        </div>
-                        <div className='col-md-6 col-right'>
-                            <p className='about-text'>
-                                Halo, Nama Saya Rudi Widodo, Lahir di Kota Karanganyar Jawa Tengah. Saya sedang belajar untuk menjadi seorang web developer. dengan bahasa pemrograman javascript dan php.
-                                saya juga membut sebuah project sederhana untuk diupload sendiri.
-                            </p>
-                            <div className='about-options'>
-                                <a href="#Contact">
-                                    <button className='btn primary-btn'>Hubungi</button>
-                                </a>
-                                <a href="#Project">
-                                    <button className='btn highlighted-btn'>Project</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="about-container" id="About">
+      <div className="about-parent">
+        <div className="about-title">
+          <h3>About</h3>
+          <div className="line"></div>
         </div>
-    )
+        <div className="about-card">
+          <div className="row">
+            <div className="col-md-6 col-left">
+              <div className="card-image-about">
+                <img
+                  src={require('../../assets/images/profile/profile.jpeg')}
+                  alt={''}
+                />
+              </div>
+            </div>
+            <div className="col-md-6 col-right">
+              <p className="about-text">
+                Halo, Nama Saya Rudi Widodo, Lahir di Kota Karanganyar Jawa
+                Tengah. Saya lulusan baru dan memiliki beberapa skil dalam
+                bahasa pemrograman. Saya berfokus pada bidang web programming,
+                dalam menggapai tujuan saya sebagai web developer, saya selalu
+                mengasah skil saya dengan membuat beberapa project pribadi.
+              </p>
+              <div className="about-options">
+                {/* <a href="#Contact">
+                                    <button className='btn primary-btn'>Hubungi</button>
+                                </a> */}
+                <a href="#Project">
+                  <button className="btn highlighted-btn">Project</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/containers/project/Project.file.js
+++ src/containers/project/Project.file.js
@@ -1,65 +1,49 @@
-import project1 from '../../assets/images/project/project-1.png';
-import project2 from '../../assets/images/project/project-2.png';
-import project3 from '../../assets/images/project/project-3.png';
-import project4 from '../../assets/images/project/project-4.png';
-import project5 from '../../assets/images/project/project-5.png';
-import project6 from '../../assets/images/project/project-6.png';
+import project1 from '../../assets/images/project/project-5.png';
+import project2 from '../../assets/images/project/project-6.png';
+import project3 from '../../assets/images/project/project-7.png';
+import project4 from '../../assets/images/project/project-8.png';
 
 const project = [
-    {
-        id: 1,
-        title: 'project - 01',
-        link: 'https://github.com/rudiwidodo069/new-sistem-akademik',
-        target: '_blank',
-        project: project1,
-        caption: `project - 01 : SISTEM AKADEMIK merupakan sebuah web apps  untuk melihat informasi yang di butuhkan mahasiswa, web apps ini di bangun dengan teknologi bhasa pemrograman php versi 7+ dengan mengunakan framework codeigniter versi 3 dengan tambahan teknologi data table untuk memper mudah penegelolaan table pada web apps tersebut.`,
-        note: `sebagai tambahan : project ini masih dalam tahap pengembahan jadi belum sepenuhnya selesai.`
-    },
-    {
-        id: 2,
-        title: 'project - 02',
-        project: project2,
-        link: 'https://github.com/rudiwidodo069/new-management-perusahaan',
-        target: '_blank',
-        caption: `project - 02 : SISTEM MANAJEMEN PERUSAHAAN merupakan sebuah web apps yang di bangun dengan teknologi bhasa pemrograman php versi 7+ dengan mengunakan framework codeigniter versi 3 dengan tambahan teknologi data table untuk memper mudah penegelolaan table pada web apps tersebut.`,
-        note: `sebagai tambahan : project ini masih dalam tahap pengembahan jadi belum sepenuhnya selesai.`
-    },
-    {
-        id: 3,
-        title: 'project - 03',
-        project: project3,
-        link: 'https://next-js-coutries-api-4sp8yqrlr.vercel.app/',
-        target: '_blank',
-        caption: `project - 03 : NEXT-JS CONTRIES API, website ini di bangun dengan teknologi framework dari next-js, dan dengan memanfaatkan sebuah public api dari contries api. yang bertujuan untuk memudahkan pengunjung website dalam mencari informasi terkait dengan sebuah ngara tersebut.`,
-        note: ``
-    },
-    {
-        id: 4,
-        title: 'project - 04',
-        project: project4,
-        link: 'https://next-project-omdb-api.vercel.app/',
-        target: '_blank',
-        caption: `project - 04 : NEXT-JS OMDB API, website ini di bangun dengan teknologi framework dari next-js, dan dengan memanfaatkan sebuah public api dari omdb api. yang bertujuan untuk memudahkan pengunjung website dalam mencari informasi terkait dengan sebuah film tersebut.`,
-        note: ``
-    },
-    {
-        id: 5,
-        title: 'project - 05',
-        project: project5,
-        link: 'https://react-covid-live-tracker.netlify.app/',
-        target: '_blank',
-        caption: 'project - 05 : REACT-COVID, website ini dibangun dengan menggunakan library react.js dan framework bootestrap. website ini ditujukan untuk mengetahui perkembangan dari coronavirus saat ini',
-        note: ''
-    },
-    {
-        id: 6,
-        title: 'project - 06',
-        project: project6,
-        link: 'https://react-netflix-clone-project.netlify.app/',
-        target: '_blank',
-        caption: 'project - 05 : REACT-NETFLIX-CLONE, website ini dibangun dengan menggunakan library react.js , framwork tailwind.css dan mengunakan firebase auth untuk user login.',
-        note: ''
-    }
+  {
+    id: 1,
+    title: 'project - 01',
+    project: project1,
+    link: 'https://react-covid-live-tracker.netlify.app/',
+    target: '_blank',
+    caption:
+      'project - 01 : REACT-COVID, website ini dibangun dengan menggunakan library react.js dan framework bootestrap. website ini ditujukan untuk mengetahui perkembangan dari coronavirus saat ini',
+    note: '',
+  },
+  {
+    id: 2,
+    title: 'project - 02',
+    project: project2,
+    link: 'https://react-netflix-clone-project.netlify.app/',
+    target: '_blank',
+    caption:
+      'project - 02 : REACT-NETFLIX-CLONE, website ini dibangun dengan menggunakan library react.js , framwork tailwind.css dan mengunakan firebase auth untuk user login. project ini hanya dibuat menggunakan api dari omdbApi.',
+    note: '',
+  },
+  {
+    id: 3,
+    title: 'project - 03',
+    project: project3,
+    link: 'https://react-typescript-hydra-landing-page.netlify.app/',
+    target: '_blank',
+    caption:
+      'project - 03 : Vite-React-TS-Hydra-Landing-Page. melakukan slicing design dari hydra landing page desain menjadi sebuah website. website ini dibuat menggunakan vite react typescript.',
+    note: '',
+  },
+  {
+    id: 4,
+    title: 'project - 04',
+    project: project4,
+    link: 'https://vite-react-tc-travel-landing-page.netlify.app/',
+    target: '_blank',
+    caption:
+      'project - 03 : Vite-React-TS-Travel-Landing-Page. melakukan slicing design dari sebuah desain di figma menjadi sebuah website. website ini dibuat menggunakan vite react typescript.',
+    note: '',
+  },
 ];
 
 export default project;
